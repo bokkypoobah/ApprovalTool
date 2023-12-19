579bbcc26539cb5eda06463c3ce7e74de97dbb5a
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2023-12-20 00:53:31 +1100

    tidy

--- docs/customNames.js
+++ docs/customNames.js
@@ -2,7 +2,9 @@ const CUSTOMNAMES = {
   "0x0000000000000000000000000000000000000000": ["eoa", "Null", "Null:0x0000...0000"],
   "0x00000000000111AbE46ff893f3B2fdF1F759a8A8": ["nftexchange", "ExecutionDelegate", "Blur:ExecutionDelegate"],
   "0x000000000022D473030F116dDEE9F6B43aC78BA3": ["erc20", "Permit2", "Uniswap:Permit2"],
+  "0x011C77fa577c500dEeDaD364b8af9e8540b808C0": ["erc721", "IPC", "ImmortalPlayerCharacter"],
   "0x0fc584529a2AEfA997697FAfAcbA5831faC0c22d": ["nftexchange", "NFTXMarketplaceZap", "NFTX:NFTXMarketplaceZap"],
+  "0x1776e1F26f98b1A5dF9cD347953a26dd3Cb46671": ["erc20", "NMR", "Numeraire"],
   "0x1E0049783F008A0085193E00003D00cd54003c71": ["nftexchange", "OS:Conduit", "OpenSea:Conduit"],
   "0x2f18F339620a63e43f0839Eeb18D7de1e1Be4DfB": ["nftexchange", "Blur:Delegate", "Blur:Delegate"],
   "0x495f947276749Ce646f68AC8c248420045cb7b5e": ["erc1155", "OSSSF", "OpenSea Shared StoreFront"],
@@ -15,13 +17,16 @@ const CUSTOMNAMES = {
   "0xA42f6cADa809Bcf417DeefbdD69C5C5A909249C0": ["erc721exchange", "NFT20CasUpgreadableV2", "NFT20:NFT20CasUpgreadableV2"],
   "0xB32979486938AA9694BFC898f35DBED459F44424": ["erc1155", "NYAN", "Nyan Cat"],
   "0xbbc53022Af15Bb973AD906577c84784c47C14371": ["nftexchange", "NFTXMarketplace0xZap", "NFTX:NFTXMarketplace0xZap"],
+  "0xBB9bc244D798123fDe783fCc1C72d3Bb8C189413": ["erc20", "DAO", "TheDAO", 16],
   "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2": ["erc20", "WETH", "Wrapped Ether", 18],
   "0xC2C747E0F7004F9E8817Db2ca4997657a7746928": ["erc721", "HM", "Hashmask"],
   "0xd07dc4262BCDbf85190C01c996b4C06a461d2430": ["erc1155", "RARI", "Rarible:RARI Token 3"],
   "0xD4416b13d2b3a9aBae7AcD5D6C2BbDBE25686401": ["erc1155", "NameWrapper", "ENS:NameWrapper"],
+  "0xE592427A0AEce92De3Edee1F18E0157C05861564": ["erc20exchange", "SwapRouter", "UniswapV3:Router"],
   "0xE5c783EE536cf5E63E792988335c4255169be4E1": ["erc721exchange", "WyvernTokenTransferProxy", "OpenSea:WyvernTokenTransferProxy"],
   "0xf42aa99F011A1fA7CDA90E5E98b277E306BcA83e": ["erc721exchange", "TransferManagerERC721", "LooksRare:TransferManagerERC721"],
   "0xF849de01B080aDC3A814FaBE1E2087475cF2E354": ["erc721exchange", "ERC721Delegate", "X2Y2:ERC721Delegate"],
+  "0xFe9231f0e6753a8412a00eC1f0028A24d5220Ba9": ["erc1155", "ZB", "ZombieBabies"],
 };
 
 const CUSTOMNAMESOLD = {
